630981307579a2e437d57238f6cff85785e6b0bf
yugogamer <57806524+yugogamer@users.noreply.github.com>
2022-02-02 14:46:54 +0100

    [func] adding a loader, an edit button and a add button(floating)

--- app/src/main/java/fr/edemart/mapeditor/MainActivity.kt
+++ app/src/main/java/fr/edemart/mapeditor/MainActivity.kt
@@ -2,21 +2,25 @@ package fr.edemart.mapeditor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ListView
 import android.widget.Toast
+import fr.edemart.mapeditor.databinding.ActivityMainBinding
 import fr.edemart.mapeditor.service.ApiClient
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.button1).setOnClickListener {
-            executeCall()
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        executeCall()
     }
     private fun executeCall() {
         GlobalScope.launch(Dispatchers.Main) {
@@ -27,12 +31,7 @@ class MainActivity : AppCompatActivity() {
                     var listView = findViewById<ListView>(R.id.listview)
                     listView.adapter = MapAdapter(this@MainActivity, maps)
 
-                    val content = response.body()
-                    Toast.makeText(
-                        this@MainActivity,
-                        content?.toString(),
-                        Toast.LENGTH_LONG
-                    ).show()
+                    binding.progressBar1.visibility = View.GONE
                 } else {
                     Toast.makeText(
                         this@MainActivity,
